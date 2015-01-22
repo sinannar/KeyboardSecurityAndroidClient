6e29f786f2eee36a49dc1ea02fff1f9bd1e2b915
sinannar <sinan.nar@gmail.com>
2015-01-22 18:09:35 +0200

    Sensor Access
    have access to accelerometer and orientation sensors

--- app/src/main/java/snnr/com/keyboardsecurityandroidclient/MainActivity.java
+++ app/src/main/java/snnr/com/keyboardsecurityandroidclient/MainActivity.java
@@ -1,39 +1,69 @@
 package snnr.com.keyboardsecurityandroidclient;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.TextView;
+import android.hardware.SensorManager;
+import android.hardware.SensorListener;
 
+public class MainActivity extends Activity implements SensorListener {
+    final String tag = "IBMEyes";
+    SensorManager sm = null;
+    TextView xViewA = null;
+    TextView yViewA = null;
+    TextView zViewA = null;
+    TextView xViewO = null;
+    TextView yViewO = null;
+    TextView zViewO = null;
 
-public class MainActivity extends ActionBarActivity {
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // get reference to SensorManager
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         setContentView(R.layout.activity_main);
+        xViewA = (TextView) findViewById(R.id.xbox);
+        yViewA = (TextView) findViewById(R.id.ybox);
+        zViewA = (TextView) findViewById(R.id.zbox);
+        xViewO = (TextView) findViewById(R.id.xboxo);
+        yViewO = (TextView) findViewById(R.id.yboxo);
+        zViewO = (TextView) findViewById(R.id.zboxo);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onSensorChanged(int sensor, float[] values) {
+        synchronized (this) {
+            Log.d(tag, "onSensorChanged: " + sensor + ", x: " +
+                    values[0] + ", y: " + values[1] + ", z: " + values[2]);
+            if (sensor == SensorManager.SENSOR_ORIENTATION) {
+                xViewO.setText("Orientation X: " + values[0]);
+                yViewO.setText("Orientation Y: " + values[1]);
+                zViewO.setText("Orientation Z: " + values[2]);
+            }
+            if (sensor == SensorManager.SENSOR_ACCELEROMETER) {
+                xViewA.setText("Accel X: " + values[0]);
+                yViewA.setText("Accel Y: " + values[1]);
+                zViewA.setText("Accel Z: " + values[2]);
+            }
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void onAccuracyChanged(int sensor, int accuracy) {
+        Log.d(tag,"onAccuracyChanged: " + sensor + ", accuracy: " + accuracy);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the orientation and accelerometer sensors
+        sm.registerListener(this,
+                SensorManager.SENSOR_ORIENTATION |SensorManager.SENSOR_ACCELEROMETER,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onStop() {
+        // unregister listener
+        sm.unregisterListener(this);
+        super.onStop();
     }
 }
